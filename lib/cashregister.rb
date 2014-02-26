9c583393bf822c599e2588aa67d6b60fe59153c5
Andrew Dixon <andrew.dixon@twitch.tv>
2014-02-26 10:36:31 -0800

    CashRegister class and specs.

--- lib/cashregister.rb
+++ lib/cashregister.rb
@@ -1,2 +1,49 @@
-class Cashregister
+require 'change'
+
+class CashRegister
+  attr_accessor :coins
+
+  def coins= (coins = [25,10,5,1])
+    if (
+      coins.class != Array ||
+      coins.map { |coin| coin.class.ancestors.include?(Integer) }.include?(false)
+    )
+      raise 'Invalid Argument'
+    end
+
+    @optimal_change = Hash.new do |optimal_change, amount|
+      optimal_change[amount] = get_optimal_change(optimal_change, amount, coins)
+    end
+
+    @coins = coins
+  end
+
+  alias :initialize :coins=
+
+  def make_change(amount)
+    if (
+      !amount.class.ancestors.include?(Integer) ||
+      amount < 0
+    )
+      raise 'Invalid Argument'
+    end
+
+    return(@optimal_change[amount])
+  end
+
+  private
+
+  def get_optimal_change(optimal_change, amount, coins)
+    if amount < coins.min
+      Change.new(coins)
+    elsif coins.include?(amount)
+      Change.new(coins).add(amount)
+    else
+      coins.map do |coin|
+        optimal_change[amount - coin].add(coin)
+      end.select do |change|
+        change.value == amount
+      end.min { |a,b| a.count <=> b.count }
+    end
+  end
 end
--- spec/cashregister_spec.rb
+++ spec/cashregister_spec.rb
@@ -1,12 +1,64 @@
 require 'spec_helper'
 require 'cashregister'
 
-describe Cashregister do
-  describe "new" do
+describe CashRegister do
+  subject(:cash_register)  { CashRegister.new }
+  let(:argument)           { [10,7,1] }
+  let(:bad_argument)       { 'A' }
+  let(:bad_array_argument) { [1,2,3,'b'] }
+
+  describe "#new" do
     it "should instantiate" do
-      lambda {
-        Cashregister.new
-      }.should_not raise_exception
+      expect { cash_register }.to_not raise_exception
+    end
+
+    its(:coins) { should eq([25,10,5,1]) }
+
+
+    it 'sets the coins when passed an argument' do
+      CashRegister.new(argument).coins.should eq([10,7,1])
+    end
+
+    it 'throws an error when passed a bad argument' do
+      expect { CashRegister.new(bad_argument) }.to raise_exception(/invalid argument/i)
+    end
+
+    it 'thows an error when passed a bad array argument' do
+      expect { CashRegister.new(bad_array_argument) }.to raise_exception(/invalid argument/i)
+    end
+  end
+
+  describe ".make_change" do
+    it 'takes a positive integer as an argument' do
+      expect { cash_register.make_change(25) }.to_not raise_exception
+    end
+
+    it 'throws an error on a negative integer argument' do
+      expect { cash_register.make_change(-25) }.to raise_exception(/invalid argument/i)
+    end
+
+    it 'thows an error on a non-integer argument' do
+      expect { cash_register.make_change('b') }.to raise_exception(/invalid argument/i)
+    end
+
+    it 'returns a hash' do
+      cash_register.make_change(25).should be_a(Hash)
+    end
+
+    it 'handles zero' do
+      cash_register.make_change(0).should eq({25 => 0, 10 => 0, 5 => 0, 1 => 0})
+    end
+
+    it 'handles the base case' do
+      cash_register.make_change(25).should eq({25 => 1, 10 => 0, 5 => 0, 1 => 0})
+    end
+
+    it 'makes change for the given amount' do
+      cash_register.make_change(26).should eq({25 => 1, 10 => 0, 5 => 0, 1 => 1})
+    end
+
+    it 'makes change for crazy foreign coins' do
+      CashRegister.new([10,7,1]).make_change(14).should eq({10 => 0, 7 => 2, 1 => 0})
     end
   end
 end
